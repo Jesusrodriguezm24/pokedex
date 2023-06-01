ed7ee3898fcaea6b1775dc1ce3339abe8a3829ef
Jesus Rodriguez <jesusrodriguezm24@gmail.com>
2023-05-31 21:24:51 -0400

    agregando header

--- src/components/Common/Layout/Layout.jsx
+++ src/components/Common/Layout/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-//import LinesTopStyle from "./LinesTopStyle/LinesTopStyle";
+import LinesTopStyle from "./LinesTopStyle/LinesTopStyle";
 import './Layout.css';
 
 const Layout = () => {
   return (
     <>
       <header>
-        {/* <LinesTopStyle/>  */}
+        <LinesTopStyle/> 
       </header>
       <main className="main_container">
         <Outlet />
--- src/components/Common/Layout/LinesTopStyle/LinesTopStyle.jsx
+++ src/components/Common/Layout/LinesTopStyle/LinesTopStyle.jsx
@@ -3,18 +3,18 @@ import './LinesTopStyle.css'
 
 const LinesTopStyle = () => {
   return (
-    <section className='line_container'>
-        <div className='line_red'>
+    <section className='line_container_top'>
+        <div className='line_red_top'>
             <img src={pokedexImg} alt="" />
         </div>
 
-        <div className='circule_out'>
-            <div className='circule_in'>
+        <div className='circule_out_top'>
+            <div className='circule_in_top'>
 
             </div>
         </div>
 
-        <div className='line_black'>
+        <div className='line_black_top'>
 
         </div>
 
